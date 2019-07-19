8e3b93ca4481d4a4b128713bfebac6a880404dab
alicialeblanc <alicia.leblanc0@gmail.com>
2019-07-19 13:48:11 +0000

    got script working

--- ps-vm.ps1
+++ ps-vm.ps1
@@ -1,16 +1,12 @@
-#variables
-$vm = 1
-$rg = test
-$user = admin
-$pass = Testing12345
-$size = Standard_B2s
+#variable
+param([String]$VMName)
 
-#make the vm
+#make the vm (figure out custom data)
 
-az vm create -n $vm -g $rg --image WinDataCenter2016 --admin-username $user --admin-password $pw --size $size 
+az vm create -n $VMName -g test --image Win2016DataCenter --admin-username admina --admin-password Testing12345 --size Standard_B2s
 
 #enable remoting
 
-Enable-AzVMPSRemoting -Name $vm -ResourceGroupName $rg -Credential $cred
+Enable-AzVMPSRemoting -Name $VMName -ResourceGroupName test -Protocol https -OsType Windows
 
 Write-Host "vm created and remoting available"
