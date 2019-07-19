ade57e5019e53cfe7e717965cda37881edf0ac50
alicialeblanc <alicia.leblanc0@gmail.com>
2019-07-19 15:01:41 +0000

    adde rgname variable

--- ps-vm.ps1
+++ ps-vm.ps1
@@ -1,12 +1,12 @@
-#variable
-param([String]$VMName)
+#variables
+param([String]$VMName, [String]$RGName)
 
-#make the vm (figure out custom data)
+#make the vm (figure out custom data), add flow control
 
-az vm create -n $VMName -g test --image Win2016DataCenter --admin-username admina --admin-password Testing12345 --size Standard_B2s
+az vm create -n $VMName -g $RGName --image Win2016DataCenter --admin-username admina --admin-password $RGNameing12345 --size Standard_B2s
 
 #enable remoting
 
-Enable-AzVMPSRemoting -Name $VMName -ResourceGroupName test -Protocol https -OsType Windows
+Enable-AzVMPSRemoting -Name $VMName -ResourceGroupName $RGName -Protocol https -OsType Windows
 
 Write-Host "vm created and remoting available"
